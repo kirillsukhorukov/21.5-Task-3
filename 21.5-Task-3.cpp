77ede53d8844bd05c301fa8c21b7adffacd28789
kirillsukhorukov <sukhorukov_kirill@mail.ru>
2023-09-21 01:14:50 +0700

    write add, subtract, scale, length function

--- 21.5-Task-3.cpp
+++ 21.5-Task-3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 struct vector
 {
@@ -8,31 +9,58 @@ struct vector
 };
 
 
-
+//Фунция сложения векторов
 void add()
 {
     vector vecA;
     vector vecB;
+    
+    vecA.x = 2.5;
+    vecA.y = 4.5;
+    vecB.x = -2.5;
+    vecB.y = 2.5;
+
+    std::cout << "Result vector coordinates: x=" << vecA.x+vecB.x << " y=" << vecA.y+vecB.y << std::endl;
 }
 
+//Фунция вычитания векторов
 void subtract()
 {
     vector vecA;
     vector vecB;
+    
+    vecA.x = 2.5;
+    vecA.y = 4.5;
+    vecB.x = -2.5;
+    vecB.y = 2.5;
+
+    std::cout << "Result vector coordinates: x=" << vecA.x-vecB.x << " y=" << vecA.y-vecB.y << std::endl;
 }
 
+//Фунция умножения вектора на скаляр
 void scale()
 {
-    vector vecA;
-    vector vecB;
+    vector vec;
+    double scale=2.0;
+
+    vec.x = 2.5;
+    vec.y = 4.5;
+    
+    std::cout << "Result vector coordinates: x=" << vec.x*scale << " y=" << vec.y*scale << std::endl;
 }
 
+//Фунция нахождения длины вектора
 void length()
 {
-    vector vecA;
-    vector vecB;
+    vector vec;
+    
+    vec.x = 1.0;
+    vec.y = 1.0;
+    
+    std::cout << "Vector length=" << sqrt(pow(vec.x,2)+pow(vec.y,2)) << std::endl;
 }
 
+//Фунция нормализации вектора
 void normalize()
 {
     vector vecA;
